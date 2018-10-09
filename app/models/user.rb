1ba0b22a497c6d89ef5954ae6ab5f1de06e4543c
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-09 18:13:48 +0900

    make basic user model

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  before_save { downcase }
+  validates :name, { presence: true, uniqueness: true, length: { maximum: 50} }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i # 大文字スタートは定数
+  validates :email, { presence: true, uniqueness: { case_sensitive: false } ,
+    length: { maximum: 255}, format: { with: VALID_EMAIL_REGEX } }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6}
+
+  
+  private
+
+    def downcase
+      email.downcase!
+    end
+
+end
