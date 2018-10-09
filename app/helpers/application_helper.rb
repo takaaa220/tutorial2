a4caf18ff6b8ede8b90ad71073b97b5dfe7afc82
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-09 16:02:55 +0900

    Finish static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(title="")
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if title.empty?
+      return base_title
+    else
+      return "#{title} | #{base_title}"
+    end
+  end
 end
