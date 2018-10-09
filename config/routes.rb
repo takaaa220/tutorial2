4dadcdbf8bacc7ebbc93473100a9ccd4766e403f
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-09 15:46:55 +0900

    routeの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get 'help' => "static_pages#help"
+  get 'about' => "static_pages#about"
 end
