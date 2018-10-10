2749bd2d7ec7b0897d273241f7e9c1a44310d1b6
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-10 16:18:11 +0900

    viewでログインし，Headerをログイン/非ログイン時で変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      flash[:success] = "ログインしました"
+      log_in @user
+      redirect_to @user
+    else
+      flash.now[:danger] = "メールアドレスとパスワードを確認してください"
+      render "new"
+    end
   end
 
   def destroy
   end
+
 end
