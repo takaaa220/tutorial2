5533dc49e7ffba9907f9ff328c33f09b42af9fde
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-10 17:39:03 +0900

    永続cookiesを用いてログイン情報を保持する機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { downcase }
   validates :name, { presence: true, uniqueness: true, length: { maximum: 50} }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i # 大文字スタートは定数
@@ -12,6 +14,22 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                 BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # return random token
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # remember token for parmanent session
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # compare remember_digest on database with remember_token
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
   private
