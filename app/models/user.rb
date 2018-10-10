afa3fb573eca1cc42b9abe8793555b50e35f1186
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-10 17:43:43 +0900

    cookies の情報を消す機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,10 @@ class User < ApplicationRecord
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
   private
 
     def downcase
