c857b066470988a57431d18a25304714dd7769c9
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-10 23:19:57 +0900

    account activation の準備

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
 
-  before_save { downcase }
   validates :name, { presence: true, uniqueness: true, length: { maximum: 50} }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i # 大文字スタートは定数
   validates :email, { presence: true, uniqueness: { case_sensitive: false } ,
@@ -39,8 +40,12 @@ class User < ApplicationRecord
 
   private
 
-    def downcase
+    def downcase_email
       self.email.downcase!
     end
 
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,14 @@
 User.create!(name: "admin", email: "admin@example.com",
             password: "adminpass",
             password_confirmation: "adminpass",
-            admin: true )
+            admin: true,
+            activated: true, activated_at: Time.zone.now )
 
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name: name, email: email,
-    password: password, password_confirmation: password)
+    password: password, password_confirmation: password,
+    activated: true, activated_at: Time.zone.now)
 end
