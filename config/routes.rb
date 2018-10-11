2777f1706d3e917fc0d3b185a6dc3b253f9a2e00
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-11 12:23:47 +0900

    generate PasswordResets controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get "signup" => "users#new"
   root 'static_pages#home'
   get 'help' => "static_pages#help"
