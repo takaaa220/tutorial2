8eea6f0495de59dabb9a1bcbcdc25bb43a569ed1
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-11 12:40:41 +0900

    パスワードの再設定

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,19 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_email
+      flash[:info] = "パスワード再設定用のメールを送信しました"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "アドレスが見つかりません"
+      render "new"
+    end
+  end
+
   def edit
   end
 end
