6f382cfa3a5e165b9a1747923a4953349f4bb09b
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-12 11:27:00 +0900

    follow, unfollowメソッドの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship",
       foreign_key: "follower_id", dependent: :destroy
-      
+  has_many :following, through: :active_relationships, source: :followed
+
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -69,6 +70,18 @@ class User < ApplicationRecord
     Micropost.where("user_id=?", id)
   end
 
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  
   private
 
     def downcase_email
