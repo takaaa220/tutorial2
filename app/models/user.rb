2d3e0842b29298616882071541049c04f7604d0a
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-12 11:12:12 +0900

    能動的な関係の定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
-
+  has_many :active_relationships, class_name: "Relationship",
+      foreign_key: "follower_id", dependent: :destroy
+      
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -66,7 +68,7 @@ class User < ApplicationRecord
   def feed
     Micropost.where("user_id=?", id)
   end
-  
+
   private
 
     def downcase_email
