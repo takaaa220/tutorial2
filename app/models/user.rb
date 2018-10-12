238c3d23cc11c7cdd3e7f10e8a4a9d3d4dd1e75b
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-12 11:30:12 +0900

    受動的なrelationshipモデルを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship",
       foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+      foreign_key: "followed_id", dependent: :destroy
+      
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
@@ -81,7 +85,7 @@ class User < ApplicationRecord
   def following?(other_user)
     following.include?(other_user)
   end
-  
+
   private
 
     def downcase_email
