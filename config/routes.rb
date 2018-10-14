3d8ad543682e0d99438342c18fe2b747377ad098
takaaa220 <taaaaaaaaaa220@gmail.com>
2018-10-14 09:39:12 +0900

    usernameにindex，users tableにin_reply_to column の追加

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
-  resources :microposts, only: [:create, :destroy]
+  resources :microposts, only: [:show, :create, :destroy]
   resources :relationships, only: [:create, :destroy]
   resources :likes, only: [:create, :destroy]
 end
